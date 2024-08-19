3042c46a8ef758bbef854ebeb4cc9b8f294c782c
tallestcat001 <shdmlgus123_@naver.com>
2024-08-19 19:43:30 +0900

    (2024.8.19) 버튼 생성

---
+++ Program/Assets/Class7th (User Interface)/Scripts/ButtonManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    [SerializeField] Text [] ButtonTexts;
+    [SerializeField] string[] ButtonNames;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
